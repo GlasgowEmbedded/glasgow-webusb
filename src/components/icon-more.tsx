5c4d3874dd4df90df16efc0e5b10abfaaf3f5994
miyuko <miyuko@calayucu.com>
2025-08-10 04:54:34 +0100

    Make the stroke width of the "more" icon thinner.

--- src/components/icon-more.tsx
+++ src/components/icon-more.tsx
@@ -4,9 +4,9 @@ import { classNames } from '../helpers/class-names';
 export const IconMore = (props: JSX.SVGAttributes<SVGSVGElement>) => {
     return (
         <svg width="16" height="16" fill="currentColor" {...props} className={classNames('icon', props.class, props.className)}>
-            <circle cx="2" cy="8" r="2" />
-            <circle cx="8" cy="8" r="2" />
-            <circle cx="14" cy="8" r="2" />
+            <circle cx="2" cy="8" r="1.5" />
+            <circle cx="8" cy="8" r="1.5" />
+            <circle cx="14" cy="8" r="1.5" />
         </svg>
     );
 };
