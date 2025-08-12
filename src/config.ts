6b453a9ff0c29f80b0828aeef3e299c365af8c22
miyuko <miyuko@calayucu.com>
2025-08-12 18:26:32 +0100

    Fix whitespace in src/config.ts.

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const HOME_DIRECTORY = '/root';
-export const MOUNT_DIRECTORY = '/mnt';export const GLASGOW_WHEEL_URL = "https://glasgow-embedded.org/latest/dist/glasgow-0.1.dev0-py3-none-any.whl";
-
+export const MOUNT_DIRECTORY = '/mnt';
+export const GLASGOW_WHEEL_URL = "https://glasgow-embedded.org/latest/dist/glasgow-0.1.dev0-py3-none-any.whl";
